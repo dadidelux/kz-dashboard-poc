9f74d900693568c9546be4a8528f02d6b622b2d4
BryanDadiz <dabryan.devopstechnologiesph@gmail.com>
2023-11-27 14:56:14 +0800

    added the sidebar

--- app.py
+++ app.py
@@ -122,7 +122,6 @@ df = pd.DataFrame(
     }
 )
 
-
 # Create a clustered bar chart
 fig = px.bar(
     df,
@@ -136,359 +135,356 @@ fig.update_layout(xaxis_title="Week", yaxis_title="Values")
 st.plotly_chart(fig)
 
 
-import streamlit as st
-import pandas as pd
+def trend_indicator_view():
+    import streamlit as st
+    import pandas as pd
 
-# Define the data with padding for missing values
-data = {
-    "Provider": [
-        "Rayna",
-        "Cheeky Monkeys",
-        "Orange Wheels",
-        "Bounce",
-        "Emaar (Main)",
-        "Ready Set Go",
-        "The Farm",
-        "Air Maniax",
-        "Playtorium",
-        "Kids HQ",
-        "Splash N Party",
-        "Dubai Garden Glow",
-        "OliOli",
-        "Woo-hoo",
-        "Adventure Parx + Cafe",
-        "Dubai Parks and Resorts",
-        "Aqua Parks Leisure",
-        "SupperClub",
-        "Emirates Park Zoo",
-        "Bricobilandia",
-        "Piccoli",
-        "Ready Steady Go",
-        "Marinelys Babysitting Center",
-        "Fiafia",
-        "Tour Dubai (Main)",
-        "Tr88house",
-        "Kids Unlimited",
-        "Dare Park",
-        "Gymnastex",
-        "Splash Island",
-        "Museum of Illusions",
-        "Cuckoo's",
-        "Kidzapp",
-        "Aventura",
-        "Tommy Life Kids Amusement Arcade",
-        "Kids Hub Entertainment",
-        "Molly Coddle Baby Spa",
-        "Playville Kids Amusement Arcade",
-        "Kids Zone (Main)",
-        "3D World Selfie Museum Dubai",
-        "Adventureland",
-        "Bricks 4 Fun",
-        "La La Land",
-        "The National Aquarium",
-        "Prosportsuae",
-        "Chillout Lounge",
-        "Gogo Village",
-        "Rose Ballet",
-        "Suwaidi Pearl Farm",
-        "Shurooq (Main)",
-        "Beitfann",
-        "Toda Dubai",
-        "Circuit X (Main)",
-        "IMG Worlds of Adventures",
-        "Swimaholic",
-        "Sahara Marine",
-        "Little Champions Club",
-        "Priohub",
-        "Swissotel Al Ghurair",
-        "Doodle Kids Play",
-        "Bab Al Nojoum",
-    ],
-    "Week 1": [
-        255,
-        167,
-        94,
-        94,
-        84,
-        54,
-        46,
-        46,
-        33,
-        62,
-        24,
-        26,
-        19,
-        20,
-        14,
-        21,
-        19,
-        15,
-        10,
-        10,
-        7,
-        12,
-        5,
-        3,
-        3,
-        8,
-        15,
-        5,
-        4,
-        3,
-        1,
-        1,
-        2,
-        0,
-        2,
-        2,
-        2,
-        0,
-        0,
-        0,
-        1,
-        0,
-        0,
-        1,
-        0,
-        0,
-        1,
-        1,
-        0,
-        0,
-        0,
-        1,
-        0,
-        1,
-    ],
-    "Week 2": [
-        241,
-        117,
-        115,
-        80,
-        57,
-        49,
-        41,
-        36,
-        37,
-        15,
-        24,
-        24,
-        22,
-        13,
-        21,
-        12,
-        12,
-        10,
-        11,
-        9,
-        8,
-        7,
-        5,
-        8,
-        3,
-        4,
-        0,
-        6,
-        3,
-        1,
-        1,
-        3,
-        3,
-        3,
-        0,
-        3,
-        3,
-        2,
-        0,
-        0,
-        3,
-        3,
-        0,
-        0,
-        1,
-        0,
-        1,
-        0,
-        2,
-        1,
-        1,
-        0,
-        0,
-        1,
-    ],
-    "Week 3": [
-        209,
-        104,
-        65,
-        53,
-        59,
-        36,
-        48,
-        34,
-        29,
-        18,
-        39,
-        20,
-        17,
-        19,
-        9,
-        8,
-        10,
-        13,
-        14,
-        13,
-        12,
-        4,
-        8,
-        6,
-        5,
-        2,
-        0,
-        0,
-        5,
-        7,
-        3,
-        3,
-        0,
-        0,
-        0,
-        1,
-        2,
-        1,
-        2,
-        0,
-        2,
-        0,
-        1,
-        3,
-        3,
-        0,
-        0,
-        0,
-        1,
-        0,
-        0,
-        0,
-        1,
-    ],
-    "Week 4": [
-        232,
-        120,
-        94,
-        55,
-        41,
-        33,
-        30,
-        34,
-        23,
-        21,
-        16,
-        22,
-        22,
-        23,
-        17,
-        15,
-        13,
-        9,
-        11,
-        10,
-        9,
-        5,
-        8,
-        3,
-        6,
-        3,
-        1,
-        2,
-        2,
-        5,
-        5,
-        3,
-        4,
-        4,
-        0,
-        1,
-        1,
-        1,
-        0,
-        1,
-        1,
-        0,
-        2,
-        1,
-        1,
-        0,
-        1,
-        1,
-        1,
-        1,
-        0,
-        1,
-    ],
-}
+    # Define the data with padding for missing values
+    data = {
+        "Provider": [
+            "Rayna",
+            "Cheeky Monkeys",
+            "Orange Wheels",
+            "Bounce",
+            "Emaar (Main)",
+            "Ready Set Go",
+            "The Farm",
+            "Air Maniax",
+            "Playtorium",
+            "Kids HQ",
+            "Splash N Party",
+            "Dubai Garden Glow",
+            "OliOli",
+            "Woo-hoo",
+            "Adventure Parx + Cafe",
+            "Dubai Parks and Resorts",
+            "Aqua Parks Leisure",
+            "SupperClub",
+            "Emirates Park Zoo",
+            "Bricobilandia",
+            "Piccoli",
+            "Ready Steady Go",
+            "Marinelys Babysitting Center",
+            "Fiafia",
+            "Tour Dubai (Main)",
+            "Tr88house",
+            "Kids Unlimited",
+            "Dare Park",
+            "Gymnastex",
+            "Splash Island",
+            "Museum of Illusions",
+            "Cuckoo's",
+            "Kidzapp",
+            "Aventura",
+            "Tommy Life Kids Amusement Arcade",
+            "Kids Hub Entertainment",
+            "Molly Coddle Baby Spa",
+            "Playville Kids Amusement Arcade",
+            "Kids Zone (Main)",
+            "3D World Selfie Museum Dubai",
+            "Adventureland",
+            "Bricks 4 Fun",
+            "La La Land",
+            "The National Aquarium",
+            "Prosportsuae",
+            "Chillout Lounge",
+            "Gogo Village",
+            "Rose Ballet",
+            "Suwaidi Pearl Farm",
+            "Shurooq (Main)",
+            "Beitfann",
+            "Toda Dubai",
+            "Circuit X (Main)",
+            "IMG Worlds of Adventures",
+            "Swimaholic",
+            "Sahara Marine",
+            "Little Champions Club",
+            "Priohub",
+            "Swissotel Al Ghurair",
+            "Doodle Kids Play",
+            "Bab Al Nojoum",
+        ],
+        "Week 1": [
+            255,
+            167,
+            94,
+            94,
+            84,
+            54,
+            46,
+            46,
+            33,
+            62,
+            24,
+            26,
+            19,
+            20,
+            14,
+            21,
+            19,
+            15,
+            10,
+            10,
+            7,
+            12,
+            5,
+            3,
+            3,
+            8,
+            15,
+            5,
+            4,
+            3,
+            1,
+            1,
+            2,
+            0,
+            2,
+            2,
+            2,
+            0,
+            0,
+            0,
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+            1,
+            1,
+            0,
+            0,
+            0,
+            1,
+            0,
+            1,
+        ],
+        "Week 2": [
+            241,
+            117,
+            115,
+            80,
+            57,
+            49,
+            41,
+            36,
+            37,
+            15,
+            24,
+            24,
+            22,
+            13,
+            21,
+            12,
+            12,
+            10,
+            11,
+            9,
+            8,
+            7,
+            5,
+            8,
+            3,
+            4,
+            0,
+            6,
+            3,
+            1,
+            1,
+            3,
+            3,
+            3,
+            0,
+            3,
+            3,
+            2,
+            0,
+            0,
+            3,
+            3,
+            0,
+            0,
+            1,
+            0,
+            1,
+            0,
+            2,
+            1,
+            1,
+            0,
+            0,
+            1,
+        ],
+        "Week 3": [
+            209,
+            104,
+            65,
+            53,
+            59,
+            36,
+            48,
+            34,
+            29,
+            18,
+            39,
+            20,
+            17,
+            19,
+            9,
+            8,
+            10,
+            13,
+            14,
+            13,
+            12,
+            4,
+            8,
+            6,
+            5,
+            2,
+            0,
+            0,
+            5,
+            7,
+            3,
+            3,
+            0,
+            0,
+            0,
+            1,
+            2,
+            1,
+            2,
+            0,
+            2,
+            0,
+            1,
+            3,
+            3,
+            0,
+            0,
+            0,
+            1,
+            0,
+            0,
+            0,
+            1,
+        ],
+        "Week 4": [
+            232,
+            120,
+            94,
+            55,
+            41,
+            33,
+            30,
+            34,
+            23,
+            21,
+            16,
+            22,
+            22,
+            23,
+            17,
+            15,
+            13,
+            9,
+            11,
+            10,
+            9,
+            5,
+            8,
+            3,
+            6,
+            3,
+            1,
+            2,
+            2,
+            5,
+            5,
+            3,
+            4,
+            4,
+            0,
+            1,
+            1,
+            1,
+            0,
+            1,
+            1,
+            0,
+            2,
+            1,
+            1,
+            0,
+            1,
+            1,
+            1,
+            1,
+            0,
+            1,
+        ],
+    }
 
-import streamlit as st
-import pandas as pd
-import plotly.express as px
+    import streamlit as st
+    import pandas as pd
+    import plotly.express as px
 
-# Define the data with padding for missing values
-# (Same data as before)
+    # Define the data with padding for missing values
+    # (Same data as before)
 
-# Ensure all weeks have the same length by padding with zeros
-max_week_length = max(len(data[week]) for week in data)
-for week in data:
-    data[week] += [0] * (max_week_length - len(data[week]))
+    # Ensure all weeks have the same length by padding with zeros
+    max_week_length = max(len(data[week]) for week in data)
+    for week in data:
+        data[week] += [0] * (max_week_length - len(data[week]))
 
-# Create a DataFrame with numerical columns
-df = pd.DataFrame(data)
+    # Create a DataFrame with numerical columns
+    df = pd.DataFrame(data)
 
+    # Convert numerical columns to integers
+    numeric_columns = df.columns[1:]  # Exclude the "Venue" column
+    df[numeric_columns] = df[numeric_columns].astype(int)
 
-# Convert numerical columns to integers
-numeric_columns = df.columns[1:]  # Exclude the "Venue" column
-df[numeric_columns] = df[numeric_columns].astype(int)
+    # Create a Streamlit app
+    st.title("Weekly Data with Trend Indicator")
 
-# Create a Streamlit app
-st.title("Weekly Data with Trend Indicator")
+    # Reset the index to include the "Venue" column
+    df.reset_index(drop=True, inplace=True)
 
-# Reset the index to include the "Venue" column
-df.reset_index(drop=True, inplace=True)
+    # Display the DataFrame with trend indicators
+    prev_week = df.columns[-2]  # Get the name of the previous week's column
+    current_week = df.columns[-1]  # Get the name of the current week's column
 
+    # Function to calculate trend indicator (Up or Down)
+    def calculate_trend_indicator(row):
+        if row[current_week] > row[prev_week]:
+            return "Up"
+        elif row[current_week] < row[prev_week]:
+            return "Down"
+        else:
+            return ""
 
-# Display the DataFrame with trend indicators
-prev_week = df.columns[-2]  # Get the name of the previous week's column
-current_week = df.columns[-1]  # Get the name of the current week's column
+    # Apply the function to each row to calculate the trend indicator
+    df["Trend"] = df.apply(calculate_trend_indicator, axis=1)
 
+    # Set the "Provider" column as the index
+    df.set_index("Provider", inplace=True)
 
-# Function to calculate trend indicator (Up or Down)
-def calculate_trend_indicator(row):
-    if row[current_week] > row[prev_week]:
-        return "Up"
-    elif row[current_week] < row[prev_week]:
-        return "Down"
-    else:
-        return ""
-
-
-# Apply the function to each row to calculate the trend indicator
-df["Trend"] = df.apply(calculate_trend_indicator, axis=1)
-
-# Set the "Provider" column as the index
-df.set_index("Provider", inplace=True)
-
-# Create a bar chart of the top 10 venues for the current week
-top_10_venues = df.nlargest(10, current_week)
-fig = px.bar(
-    top_10_venues,
-    x=top_10_venues.index,
-    y=current_week,
-    title="Top 10 Providers for Current Week",  # Updated title
-)
-fig.update_xaxes(title="Provider")
-fig.update_yaxes(title=current_week)
-# Display the table with trend indicators
-#st.write(df)
-st.dataframe(df, use_container_width=True)
-st.plotly_chart(fig)
+    # Create a bar chart of the top 10 venues for the current week
+    top_10_venues = df.nlargest(10, current_week)
+    fig = px.bar(
+        top_10_venues,
+        x=top_10_venues.index,
+        y=current_week,
+        title="Top 10 Providers for Current Week",  # Updated title
+    )
+    fig.update_xaxes(title="Provider")
+    fig.update_yaxes(title=current_week)
+    # Display the table with trend indicators
+    # st.write(df)
+    st.dataframe(df, use_container_width=True)
+    st.plotly_chart(fig)
 
 
 # ================================================================ KEYWORD VIEWS =================================================================
@@ -584,31 +580,35 @@ def to_csv(df):
     return df.to_csv().encode("utf-8")
 
 
-st.title("CleverTap Keyword Analysis")
+def keyword_view():
+    st.title("CleverTap Keyword Analysis")
 
-# Date pickers for start and end date
-start_date = st.date_input("Start Date")
-end_date = st.date_input("End Date")
+    # Date pickers for start and end date
+    start_date = st.date_input("Start Date")
+    end_date = st.date_input("End Date")
 
-if st.button("Fetch Keywords"):
-    # Convert dates to required format
-    from_date = start_date.strftime("%Y%m%d")
-    to_date = end_date.strftime("%Y%m%d")
+    if st.button("Fetch Keywords"):
+        # Convert dates to required format
+        from_date = start_date.strftime("%Y%m%d")
+        to_date = end_date.strftime("%Y%m%d")
 
-    keywords = fetch_keywords_from_clevertap(from_date, to_date)
-    if keywords:
-        display_wordcloud(keywords)
-        df = convert_to_dataframe(keywords)
-        st.dataframe(df, use_container_width=True)  # Display the DataFrame in Streamlit
+        keywords = fetch_keywords_from_clevertap(from_date, to_date)
+        if keywords:
+            display_wordcloud(keywords)
+            df = convert_to_dataframe(keywords)
+            st.dataframe(
+                df, use_container_width=True
+            )  # Display the DataFrame in Streamlit
 
-        # Create a download button for the CSV
-        csv = to_csv(df)
-        st.download_button(
-            label="Download data as CSV",
-            data=csv,
-            file_name="keywords.csv",
-            mime="text/csv",
-        )
+            # Create a download button for the CSV
+            csv = to_csv(df)
+            st.download_button(
+                label="Download data as CSV",
+                data=csv,
+                file_name="keywords.csv",
+                mime="text/csv",
+            )
+
 
 # ================================================================ PRODUCT VIEWS =================================================================
 
@@ -617,9 +617,15 @@ if st.button("Fetch Keywords"):
 def fetch_product_views(from_date, to_date):
     EVENT_NAME = "Product Viewed"
 
+    # headers = {
+    #     "X-CleverTap-Account-Id": os.getenv("CLEVERTAP_ACCOUNT_ID"),
+    #     "X-CleverTap-Passcode": os.getenv("CLEVERTAP_PASSCODE"),
+    #     "Content-Type": "application/json",
+    # }
+
     headers = {
-        "X-CleverTap-Account-Id": os.getenv("CLEVERTAP_ACCOUNT_ID"),
-        "X-CleverTap-Passcode": os.getenv("CLEVERTAP_PASSCODE"),
+        "X-CleverTap-Account-Id": st.secrets["CLEVERTAP_ACCOUNT_ID"],
+        "X-CleverTap-Passcode": st.secrets["CLEVERTAP_PASSCODE"],
         "Content-Type": "application/json",
     }
 
@@ -692,10 +698,25 @@ def display_horizontal_bar_chart(data):
         st.plotly_chart(fig, use_container_width=True)
 
 
-st.title("CleverTap Experience View Analysis")
+def experience_view():
+    st.title("CleverTap Experience View Analysis")
+    # Date pickers for start and end date
+    start_date = st.date_input("Start Date")
+    end_date = st.date_input("End Date")
 
-if st.button("Fetch Experience Views"):
-    from_date = start_date.strftime("%Y%m%d")
-    to_date = end_date.strftime("%Y%m%d")
-    data = fetch_product_views(from_date, to_date)
-    display_horizontal_bar_chart(data)
+    if st.button("Fetch Experience Views"):
+        from_date = start_date.strftime("%Y%m%d")
+        to_date = end_date.strftime("%Y%m%d")
+        data = fetch_product_views(from_date, to_date)
+        display_horizontal_bar_chart(data)
+
+
+page_names_to_funcs = {
+    "Experience View": experience_view,
+    "Keyword View": keyword_view,
+    "Trend View ": trend_indicator_view,
+}
+
+demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+st.sidebar.write("To view the other pages scroll the content down ➡️")
+page_names_to_funcs[demo_name]()
